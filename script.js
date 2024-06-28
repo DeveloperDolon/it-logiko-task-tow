613c624de40afd5623bd4170f3a8e6d8225addcd
Dolon Chandra Roy <dolonr718@gmail.com>
2024-06-29 04:27:48 +0600

    one direction moving added commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+const little_box = document.getElementById("little-box"),
+  box_container = document.getElementById("container");
+
+const containerSize = 100;
+const boxSize = 20;
+
+let x = 0;
+let y = 0;
+let dx = 1;
+let dy = 1;
+
+function moveBox() {
+  x += dx;
+  y += dy;
+
+  if (x <= 0 || x >= containerSize - boxSize) {
+    dx *= -1;
+  }
+  if (y <= 0 || y >= containerSize - boxSize) {
+    dy *= -1;
+  }
+
+  little_box.style.left = x + "px";
+  little_box.style.top = y + "px";
+
+  requestAnimationFrame(moveBox); // to ensure smooth animation for this project
+}
+
+moveBox();
