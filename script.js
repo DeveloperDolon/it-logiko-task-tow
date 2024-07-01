d8d01049496c603fc4a40a42436f0dba42db38c5
Dolon Chandra Roy <dolonr718@gmail.com>
2024-07-01 19:32:00 +0600

    project complete commit

--- script.js
+++ script.js
@@ -3,27 +3,48 @@ const little_box = document.getElementById("little-box"),
 
 const containerSize = 100;
 const boxSize = 20;
-
-let x = 0;
+let xReverse = false;
+let yReverse = false;
+let x = 50;
 let y = 0;
-let dx = 1;
-let dy = 1;
 
-function moveBox() {
-  x += dx;
-  y += dy;
+function movingBox() {
+  if (x >= 0 && x <= containerSize - boxSize) {
+    if (x >= 0 && !xReverse) {
+      x++;
+      if (x === containerSize - boxSize) {
+        xReverse = true;
+      }
+    }
 
-  if (x <= 0 || x >= containerSize - boxSize) {
-    dx *= -1;
+    if (x > 0 && xReverse) {
+      x--;
+      if (x === 1) {
+        xReverse = false;
+      }
+    }
   }
-  if (y <= 0 || y >= containerSize - boxSize) {
-    dy *= -1;
+
+  if (y >= 0 && y <= containerSize - boxSize) {
+    if (y >= 0 && !yReverse) {
+      y++;
+      if (y === containerSize - boxSize) {
+        yReverse = true;
+      }
+    }
+
+    if (y > 0 && yReverse) {
+      y--;
+      if (y === 1) {
+        yReverse = false;
+      }
+    }
   }
 
   little_box.style.left = x + "px";
   little_box.style.top = y + "px";
 
-  requestAnimationFrame(moveBox); // to ensure smooth animation for this project
+  requestAnimationFrame(movingBox);
 }
 
-// moveBox();
+movingBox();
